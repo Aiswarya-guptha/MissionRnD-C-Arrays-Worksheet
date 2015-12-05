dedf54d3e969c108a18be110f8923b478888b3d7
aiswarya-guptha <aiswaryaguptha36@gmail.com>
2015-12-05 12:17:10 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -15,5 +15,24 @@ NOTES: Don't create new array, try to change the input array.
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	return NULL;
+	if (Arr==NULL||len<0)
+		return NULL;
+	int arr[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int pos, iter;
+	for (pos = 0, iter = 0; iter < len; iter++){
+		if (arr[Arr[iter]] == 1){
+			//skip if repeatation occurs
+			continue;
+		}
+		if (iter != pos){
+			//shift when repeatation occured atleast once
+			Arr[pos] = Arr[iter];
+			pos++;
+			arr[Arr[iter]] = 1;
+		}
+		else{
+			arr[Arr[iter]] = 1;
+			pos++;
+		}
+	}
 }
